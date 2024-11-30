e58599928f9240c0b83eba20e7754da3399e95e1
chenliee <1067242400@qq.com>
2024-11-30 14:06:25 +0800

    add location package

---
+++ amap_location_fluttify-0.22.0/example/android/app/src/main/java/me/yohom/amap_location_fluttify_example/MainActivity.java
@@ -0,0 +1,6 @@
+package example.android.app.src.main.java.me.yohom.amap_location_fluttify_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
